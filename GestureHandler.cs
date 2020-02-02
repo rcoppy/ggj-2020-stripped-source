3414c8916ae8c4a6082c685c89822edc4125867b
Alex Rupp-Coppi <alex@dossamer.io>
2020-02-02 14:11:09 -0500

    init

---
+++ GestureHandler.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class GestureHandler : MonoBehaviour
+{
+    public UnityEvent horseRearEvent;
+    public UnityEvent pandaFallEvent;
+
+    void Start()
+    {
+        NuitrackManager.onNewGesture += NuitrackManager_onNewGesture;
+
+        if (horseRearEvent == null)
+            horseRearEvent = new UnityEvent();
+
+        if (pandaFallEvent == null)
+            pandaFallEvent = new UnityEvent();
+
+        // horseRearEvent.AddListener(Ping);
+    }
+
+    private void OnDestroy()
+    {
+        NuitrackManager.onNewGesture -= NuitrackManager_onNewGesture;
+    }
+
+    private void NuitrackManager_onNewGesture(nuitrack.Gesture gesture)
+    {
+        if ((gesture.Type == nuitrack.GestureType.GestureSwipeRight || gesture.Type == nuitrack.GestureType.GestureSwipeLeft)
+            && horseRearEvent != null) {
+            horseRearEvent.Invoke();
+            Debug.Log("swipe detected");
+        } else if (gesture.Type == nuitrack.GestureType.GesturePush
+            && pandaFallEvent != null)
+        {
+            pandaFallEvent.Invoke();
+            Debug.Log("push detected");
+        }
+    }
+}
